f049fb8cc655c445ee2f118adac415e7f66758c5
Emre Kara <emrekara.dev@gmail.com>
2022-09-30 22:00:57 +0300

    edit

--- frontend/src/redux/api/constants.ts
+++ frontend/src/redux/api/constants.ts
@@ -1,2 +1,2 @@
-export const BACKEND_ROOT_URL = process.env.REACT_APP_ROOT_URL_BACKEND;
+export const BACKEND_ROOT_URL = process.env.REACT_APP_ROOT_URL_BACKEND || "http://localhost:4000";
 export const FRONTEND_ROOT_URL = process.env.REACT_APP_ROOT_URL_FRONTEND;
