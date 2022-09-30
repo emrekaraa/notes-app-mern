4ea46d02cc834948e7287646d2e16ec27a8133d8
Emre Kara <emrekara.dev@gmail.com>
2022-09-30 19:02:06 +0300

    prod

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ mongoose
     console.log("Connected to MongoDB");
 
     app.listen(process.env.PORT || 4000, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
+      console.log(`Server is running on port ${process.env.PORT || 4000}`);
     });
   })
   .catch((error) => {
